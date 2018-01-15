77a3fd059f5146f6b85f2c5d7715e04c040d5601
shingfei <0915328@hr.nl>
2018-01-15 19:51:52 +0100

    Remembers pincode

--- app/src/main/java/com/example/sf/testapp/pincodeActivity.java
+++ app/src/main/java/com/example/sf/testapp/pincodeActivity.java
@@ -1,6 +1,7 @@
 package com.example.sf.testapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,10 +21,73 @@ public class pincodeActivity extends AppCompatActivity {
     private PinLockView mPinLockView;
     private IndicatorDots mIndicatorDots;
     private TextView errorLabel;
-    private boolean pinCreated = false;
-    private int storedPin = 0;
+    private boolean pinCreated;
+    private boolean pinSaved;
+    private int savedPincode;
+    private String pincode = "pincode";
+    private String pinCreate = "pinCreate";
+    private SharedPreferences pincodeSaved;
+    private SharedPreferences pincodeCreated;
 
-    //Main activity declaration
+
+    //On create events
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pincode);
+        pincodeSaved = getPreferences(MODE_PRIVATE);
+        pincodeCreated = getPreferences(MODE_PRIVATE);
+        //Shared viarable
+        pinCreated = pincodeCreated.getBoolean(pinCreate,false);
+        savedPincode = pincodeSaved.getInt(pincode,0);
+
+        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
+        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
+        errorLabel = (TextView) findViewById(R.id.profile_name);
+        Button changePin = (Button) findViewById(R.id.changePin);
+        Button logOffPin = (Button) findViewById(R.id.logoffPin);
+
+        mPinLockView.attachIndicatorDots(mIndicatorDots);
+        mPinLockView.setPinLockListener(mPinLockListener);
+        SharedPreferences sharedPref= getSharedPreferences("mypref", 0);
+
+        if(pinCreated == false)
+        {
+            errorLabel.setText("Enter a new pin");
+        }
+        else
+        {
+            errorLabel.setText("Enter your pin");
+        }
+
+        //Reset pin button
+        changePin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                pincodeSaved.edit().clear().commit();
+                pincodeCreated.edit().clear().commit();
+                pinCreated = false;
+                loginActivity(view);
+            }
+        });
+
+        //Logoff button
+        logOffPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                loginActivity(view);
+                finish();
+            }
+        });
+
+
+    }
+
     private void mainActivity()
     {
         Intent mainMenuActivity = new Intent(this,mainMenu.class);
@@ -42,23 +106,23 @@ public class pincodeActivity extends AppCompatActivity {
     //A pincode checker if the pincode has been made or is correct.
     private PinLockListener mPinLockListener = new PinLockListener()
     {
-
         @Override
         public void onComplete(String pin) {
 
             Log.d(TAG, "Pin complete: " + pin);
             int check = Integer.parseInt(pin); //Parsing since the library doesn't have any other way to recieve the pin
 
-            if(check == storedPin && pinCreated == true)
+            if(pincodeSaved.getInt(pincode,savedPincode) == check && pincodeCreated.getBoolean(pinCreate,pinSaved) == true)
             {
                 mainActivity(); // Starting new activity
                 finish(); //Ends previous activity
             }
             else
             {
-                if(pinCreated == true) //Check if the pin is created and if its incorrect
+                if(pincodeCreated.getBoolean(pinCreate,pinSaved) == true) //Check if the pin is created and if its incorrect
                 {
                     errorLabel.setText("Try again");
+                    System.out.println(pincodeSaved.getInt(pincode,savedPincode));
                     mPinLockView.resetPinLockView();//Resetting the pinview
                     System.out.println("Error");
                 }
@@ -73,9 +137,11 @@ public class pincodeActivity extends AppCompatActivity {
         {
             int checkPin = Integer.parseInt(pin);
 
-            if(storedPin == checkPin)
+            if(pincodeSaved.getInt(pincode,savedPincode) == checkPin)
             {
-                pinCreated = true; //allowing the normal pinchecker to do his job
+                pinSaved = true;
+                pincodeCreated.edit().putBoolean(pinCreate, pinSaved).commit();
+                //allowing the normal pinchecker to do his job
                 System.out.println("stored worked");
                 errorLabel.setText("Voer uw pin");
                 mPinLockView.resetPinLockView();
@@ -83,9 +149,11 @@ public class pincodeActivity extends AppCompatActivity {
             }
             else if(checkPin>0) //Checking if the user filled in the created pin and gets compared to the stored pincode
             {
-                    storedPin = checkPin;
+                    savedPincode = checkPin;
+                    pincodeSaved.edit().putInt(pincode,savedPincode).commit();
+
                     mPinLockView.resetPinLockView();
-                    //System.out.println("checkin creating pin" + checkPin + " "+storedPin);
+                    System.out.println("checkin creating pin" + checkPin + " "+pincodeSaved.getInt(pincode,checkPin));
                     errorLabel.setText("Re enter pin");
             }
             else
@@ -120,51 +188,6 @@ public class pincodeActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
-    //On create events
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pincode);
-        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
-        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
-        errorLabel = (TextView) findViewById(R.id.profile_name);
-        Button changePin = (Button) findViewById(R.id.changePin);
-        Button logOffPin = (Button) findViewById(R.id.logoffPin);
 
-        mPinLockView.attachIndicatorDots(mIndicatorDots);
-        mPinLockView.setPinLockListener(mPinLockListener);
-
-        if(pinCreated == false)
-        {
-            errorLabel.setText("Enter a new pin");
-        }
-        else
-        {
-            errorLabel.setText("Enter your pin");
-        }
-
-        //Reset pin button
-        changePin.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                pinCreated = false;
-                loginActivity(view);
-            }
-        });
-
-        //Logoff button
-        logOffPin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-
-                loginActivity(view);
-                finish();
-            }
-        });
-    }
 
 }
